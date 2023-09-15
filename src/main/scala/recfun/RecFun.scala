75f99878e8924eb8ffc6da18f876bae5739790bd
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-09-15 14:53:40 +0300

    UPD: reworked the balance func with if-else

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -26,13 +26,9 @@ object RecFun extends RecFunInterface:
   def balance(chars: List[Char]): Boolean =
     def loop(acc: Int, chars: List[Char]): Boolean =
       if chars.isEmpty then acc == 0
-      else
-        chars.head match
-          case '(' => loop(acc + 1, chars.tail)
-          case ')' =>
-            if acc == 0 then return false
-            else loop(acc - 1, chars.tail)
-          case _ => loop(acc, chars.tail)
+      else if chars.head == '(' then loop(acc + 1, chars.tail)
+      else if chars.head == ')' then acc > 0 && loop(acc - 1, chars.tail)
+      else loop(acc, chars.tail)
 
     loop(0, chars)
 
